6e59fb2e5a144d39ce97ee9074b71cb4266f862e
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-05-17 14:00:23 +0100

    fix nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 
-const Nav = () => (
-  <header>
-    <h1>Space Travelers&apos; Hub</h1>
+const Nav = () => {
+  return (
+    <header>
+    <h1>Space Travelers' Hub</h1>
     <ul>
-      <li>
-        <Link to="/">Rockets</Link>
-      </li>
-      <li>
-        <Link to="/missions">Missions</Link>
-      </li>
-      <li>
-        <Link to="/profile">Profile</Link>
-      </li>
+        <li>
+            <Link to="/">Rockets</Link>
+        </li>
+        <li>
+            <Link to="/MyProfil">My Profil</Link>
+        </li>
+        <li>
+            <Link to="/Missions">Missions</Link>
+        </li>
     </ul>
-  </header>
-);
+    </header>
+  )
+}
 
 export default Nav;
