a7435683cd0f215150ac4749092d1f9e35ccbdab
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-05-17 11:05:40 +0200

    implement nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyProfil from "./components/Rockets/Rockets";
+import Rockets from "./components/Myprofil/MyProfil";
+import Missions from "./components/Missions/Missions";
+import Nav from "./components/Nav/nav";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/Missions" element={<Missions />} />
+          <Route path="/Myprofil" element={<MyProfil />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Missions/Missions.js
@@ -0,0 +1,10 @@
+const Missions = () => (
+    <>
+   <h1>
+       Missions
+   </h1>
+   <p>Mission 1, Mission 2</p>
+   </>
+)
+
+export default Missions;
