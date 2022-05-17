65a921405e0a09133e03dc53383c067e9b59512f
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-05-17 15:42:16 +0200

    add reducers

--- src/Redux/Rockets/Rockets.js
+++ src/Redux/Rockets/Rockets.js
@@ -1,18 +1,31 @@
-import axios from "axios";
+import { Action } from "history";
 
 export const FETCH_ROCKETS = 'space-travelers-hub/Rockets/FETCH_ROCKETS';
 export const url = 'https://api.spacexdata.com/v3/rockets';
 export const initialState = [];
 
+//on fetched book 
+
+const onSuccess = (rockets) => ({
+    type: FETCH_ROCKETS,
+    playload: rockets
+})
+
 //Fetched rocket action
 
-const fetchRocketsAction = () => async (dispatch)=>{
-    const response = await axios.get(url);
-    const fetchedRockets = Object.entries(response.data).map((item) => {
-        const { id, rocket_name, description, flickr_images } = item[0];
-        return {
-            id
-        }
-    })
+const fetchRocketsAction = () => async (dispatch) =>{
+    const response = await fetch(url);
+    const rocketsData = await response.json();
+    const fetchedRockets = rocketsData.map((rocket) => ({
+        id: rocket[0],
+        rocket_name: rocket[0].rocket_name,
+        description: rocket[0].flickr_images
+    }))
+    dispatch(onSuccess(fetchedRockets));
 }
+
+const rocketsReducer = (state = initialState, action) => {
+    
+}
+
 export default fetchRocketsAction;
--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,10 +1,47 @@
-const Rockets = () => (
-     <>
-    <h1>
-        Rockets
-    </h1>
-    <p>Rocket 1, Rocket 2</p>
+const Rockets = () => {
+  return (
+    <>
+      <div className="card-container">
+        <div className="card">
+          <div className="rocket-img">
+            <img src="" alt="rocket" />
+          </div>
+          <div className="rocket-description">
+            <h1>Rocket1</h1>
+            <p> short description of the rocket </p>
+            <button className="reserve-rocket" type="button">
+              Reserve Rocket
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="rocket-img">
+            <img src="" alt="rocket" />
+          </div>
+          <div className="rocket-description">
+            <h1>Rocket2</h1>
+            <p> short description of the rocket </p>
+            <button className="reserve-rocket" type="button">
+              Reserve Rocket
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="rocket-img">
+            <img src="" alt="rocket" />
+          </div>
+          <div className="rocket-description">
+            <h1>Rocket3</h1>
+            <p> short description of the rocket </p>
+            <button className="reserve-rocket" type="button">
+              Reserve Rocket
+            </button>
+          </div>
+        </div>
+      </div>
+      <p>Rocket 1, Rocket 2</p>
     </>
-)
+  );
+};
 
 export default Rockets;
